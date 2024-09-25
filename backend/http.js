4cafa7c3175da659a37cf748508fb7b7ea05bd78
Isaac Pereira do Prado <icpereiradoprado@gmail.com>
2024-09-24 22:05:11 -0300

    Configura o servidor para aceitar requisições longas

--- backend/http.js
+++ backend/http.js
@@ -11,7 +11,8 @@ const serverHttp = createServer(app);
 const io = new Server(serverHttp);
 
 //Config JSON response
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const ipv4 = process.env.IPV4;
 const port = process.env.PORT || 5000;
