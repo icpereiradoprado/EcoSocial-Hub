3f30b2913f873d6cf81d39a8e1c079217c9786df
IgorDSouza <igor251999@hotmail.com>
2024-10-23 12:53:16 -0300

    alteração estilo pontos de coleta e community

--- EcohubMobile/src/screens/CommunityScreen.jsx
+++ EcohubMobile/src/screens/CommunityScreen.jsx
@@ -1,10 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Image, Text, Dimensions, FlatList, Button, TextInput} from "react-native";
+import { base, colors } from "../css/base";
+
 export function CommunityScreen(){
     return(
-        <View style={{backgroundColor: 'red', flex: 1}}>
-            <Text>
-                Community
-            </Text>
+        <View >
+         
+             <View style={[base.flexRow, {justifyContent: "space-between", alignItems: "center", paddingHorizontal: 24, width: '100%'}]}>
+                <View style={[base.flexRow]}>
+                    <Text style={[base.title,{paddingTop:13}]}>Comunidade  </Text>
+                    <Image source={require('../assets/images/community.png')} style = {styles.logo} />
+                </View>
+                <TouchableOpacity style={styles.criarContent} onPress={()=> {setModalVisible(true); setMode(Mode.create)}}>
+                     <TextInput 
+                     style={[
+                        styles.input,
+                      ]}
+                     placeholder="Criar publicação"
+                     onFocus={() => {setModalVisible(true); setMode(Mode.create)}}                     
+                     /> 
+                </TouchableOpacity>
+            </View>
+            
         </View>
     )
 }
+const styles = StyleSheet.create({
+    
+    logo:{
+        marginTop:10,
+        width:48,
+        height:48,
+    },
+    criarContent:{
+        paddingTop:13
+    },
+
+    input:{
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    width:'120%',
+    textAlign:'center',
+    
+    }
+});
--- EcohubMobile/src/screens/PlacesScreen.jsx
+++ EcohubMobile/src/screens/PlacesScreen.jsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Image, Text, Dimensions, FlatList, Button, TextInput} from "react-native";
+import { base, colors } from "../css/base";
+
 export function PlacesScreen(){
     return(
-        <View>
-            <Text>
-                Places
-            </Text>
+        <View >
+             <View style={[base.flexRow, {justifyContent: "space-between", alignItems: "center", paddingHorizontal: 24, width: '100%'}]}>
+                <View style={[base.flexRow]}>
+                    <Text style={[base.title,{paddingTop:13}]}>Pontos de Coleta </Text>
+                    <Image source={require('../assets/images/pontoColeta.png')} style = {styles.logo} />
+                </View>
+            </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    
+    logo:{
+        marginTop:15,
+        width:45,
+        height:45,
+    },
+    input:{
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    width:'120%',
+    textAlign:'center',
+    
+    }
+});
